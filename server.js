213170f5af9e94516c82dd47ee9159753d7645b4
yishai <yishai.nachaliel@orcam.com>
2022-04-27 08:09:00 +0300

    step 2: ex 3.1

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const app = express();
 const PORT = 5454;
 const fs = require("fs");
 const path = require("path");
+const dataPath = __dirname + "/./data/tasks.json";
 
 app.listen(PORT, () => {
   console.log("server listening on port ", PORT);
+});
+
+app.get("/allTasks", (req, res) => {
+  const data = fs.createReadStream(path.join(dataPath), "utf-8");
+  data.pipe(res);
+});
+app.delete("/allTasks", (req, res) => {
+  fs.writeFileSync(dataPath, "[]");
+  res.send("deleted");
 });
